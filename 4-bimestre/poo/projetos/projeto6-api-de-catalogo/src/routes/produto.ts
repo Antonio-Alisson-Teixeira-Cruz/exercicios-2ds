0f9fc8be00c0e9aa7da238b450b269b516528bb5
Raven Teixeira Cruz <ravenjouki@gmail.com>
2025-11-19 08:08:27 -0300

    Update produto.ts

--- 4-bimestre/poo/projetos/projeto6-api-de-catalogo/src/routes/produto.ts
+++ 4-bimestre/poo/projetos/projeto6-api-de-catalogo/src/routes/produto.ts
@@ -8,14 +8,12 @@ const myProductArr = [
   { id: 3, nome: "Monitor Full HD", preco: 900 }
 ];
 
-// Rota para listar todos os produtos
 router.get("/", (req, res) => {
   res.json( [{ id: 1, nome: "Mouse Gamer", preco: 120 },
   { id: 2, nome: "Teclado Mecânico", preco: 350 },
   { id: 3, nome: "Monitor Full HD", preco: 900 }] )
 });
 
-// Rota para pegar um produto específico pelo ID
 router.get("/:id", (req, res) => {
   const id = parseInt(req.params.id);
   const produto = myProductArr.find(p => p.id === id);
